d4934de444d6a3125a1f0e05d01ed08764392f76
fzrsahi <fazrul.anugrah17@gmail.com>
2022-09-10 23:38:45 +0800

    coomit

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const pluto = document.getElementsByClassName("planet");
+pluto[4].style.marginRight = "50px"
+
+
+const aboutButton = document.getElementsByTagName("a")
+aboutButton[1].innerHTML ="About";
